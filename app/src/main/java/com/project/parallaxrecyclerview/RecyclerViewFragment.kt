58588c03b46804a84dfa193ecf819b717895dbe3
손성빈 <sonseongbin@sonseongbin-ui-MacBook-Pro.local>
2017-11-25 17:44:34 +0900

    0.0.1 버전

--- app/src/main/java/com/project/parallaxrecyclerview/RecyclerViewFragment.kt
+++ app/src/main/java/com/project/parallaxrecyclerview/RecyclerViewFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -50,8 +49,7 @@ class RecyclerViewFragment : Fragment() {
                     translationY = heightToScroll
                 }
 
-                if(translationY <= heightToScroll) {
-                    Log.e(heightToScroll.toString(), translationY.toString())
+                if  (translationY <= heightToScroll) {
                     listener?.onScrollHeader(-translationY)
                 }
 
